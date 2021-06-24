000cb15785c4b605b22803116051d7c777d001b8
SiumBN <jsjomsita@gmail.com>
2021-06-24 18:44:07 +0600

    gpa added

---
+++ gpa.py
@@ -0,0 +1,31 @@
+
+a = int (input("Enter Marks : "))
+
+
+if  (a>=80 and a<=100):
+    print("A+")
+elif (a>=75 and a<=79):
+    print("A")
+elif (a>=70 and a<=74):
+    print("A-")
+elif (a>=65 and a<=69):
+    print("B+")
+elif (a>=60 and a<=64):
+    print("B")
+elif (a>=55 and a<=59):
+    print("B-")
+elif (a>=50 and a<=54):
+    print("C+")
+elif (a>=45 and a<=49):
+    print("C")
+elif(a>=40 and a<=44):
+    print("D")
+elif(a>=0 and a<=39):
+    print("F")
+else:
+    print("Error Marks!")
+
+
+
+
+
